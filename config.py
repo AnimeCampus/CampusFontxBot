d82154af0d797a67f992d28acb13904eb424a181
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-10 17:20:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      API_ID = int(os.environ.get("API_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      OWNER_ID = int(os.environ.get("OWNER_ID"))
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "6065321925:AAE447qFGOZBhMJeDr413xE1d1_C8Jbbqno")
+      API_ID = int(os.environ.get("API_ID", 16743442))
+      API_HASH = os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
+      OWNER_ID = int(os.environ.get("OWNER_ID", "6198858059"))
 
