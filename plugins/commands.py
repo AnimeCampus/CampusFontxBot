fc3b0488e86d7fbd310de0eca466c26a112fd60b
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-10 17:23:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,16 +4,15 @@ from .fonts import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-
 @Client.on_message(filters.command('start'))
 async def start(c, m):
     owner = await c.get_users(int(Config.OWNER_ID))
-    owner_username = owner.username if owner.username else 'riz4d'
+    owner_username = owner.username if owner.username else 'GenXNano'
 
     # start text
     text = f"""Hey! {m.from_user.mention(style='md')},
 
- ** I am Font Bot**
+ ** I am Campus Font Bot**
 
 `I can help you to get fonts. Just send me some text.`
 """
@@ -21,7 +20,7 @@ async def start(c, m):
     # Buttons
     buttons = [
         [
-            InlineKeyboardButton('Developer', url=f"https://telegram.me/riz4d")
+            InlineKeyboardButton('Developer', url=f"GenXNano.t.me")
         ]
     ]
     await m.reply_text(
